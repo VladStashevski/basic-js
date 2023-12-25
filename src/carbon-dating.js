e3c6c9f4f5e4a332715e370f21c85804f59a6647
Vladislav Lushnikov <adrenaline2142@gmail.com>
2023-12-26 00:53:32 +0500

    feat: carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,20 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (!sampleActivity || typeof sampleActivity !== 'string') {
+    return false;
+  }
+
+  const numericActivity = parseFloat(sampleActivity);
+
+  if (isNaN(numericActivity) || numericActivity <= 0 || numericActivity > 15) {
+    return false;
+  }
+
+  const firstOrder = Math.log(MODERN_ACTIVITY / numericActivity);
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  return Math.ceil(firstOrder / k);
 }
 
 module.exports = {
