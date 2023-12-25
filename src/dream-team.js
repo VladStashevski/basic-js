05acac775c55f15fd3bf7b6274c62ecc30dfaa11
Vladislav Lushnikov <adrenaline2142@gmail.com>
2023-12-26 01:04:14 +0500

    refactor: dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,35 +1,29 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
- function createDreamTeam(members) {
-  let assent = [];
-  
-if(Array.isArray(members)) {
-    
-  for(member of members) {
-      
-     if(typeof member === 'string') {
-        
-      assent.push(member.trim()[0].toUpperCase())
-      }
-    }
-  return assent.sort().join('')
-  }
-return false
+function createDreamTeam(members) {
+  return (
+    Array.isArray(members) &&
+    members
+      .filter((m) => typeof m === "string")
+      .map((m) => m.trim()[0].toUpperCase())
+      .sort()
+      .join("")
+  );
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
